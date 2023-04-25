580dc875f6f3332052485e94fdfc5823a3762d6b
ADHHHH <adekunledh@gmail.com>
2023-04-25 11:14:40 -0500

    nnn

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,10 @@ function App() {
   const [items, setItems] = useState([]);
 
   let input = null;
-
   let inputRef = useRef()
+
+ 
+  
 
 
 
@@ -20,11 +22,18 @@ function App() {
   let inputTodo = function (event) {
      
      input = event.target.value;
+     
+     
+
   }
 
   let submitTodo = function (){
     setItems([...items, input]);
+    
      inputRef.current.value = "";
+     
+     
+    
   }
   let deleteTodo = function (indexToDelete) {
      let deletedItem = items.filter((_,index) => index != indexToDelete);
@@ -38,11 +47,14 @@ let editTodo = function (index) {
   return (
     <div className="App">
       <div class="container">
-        <h1>Create your Todo-list</h1>
+        <h3>Create your Todo-list</h3>
+        <div class="top-div">
         <input ref={inputRef} onChange={inputTodo}
         placeholder="What are your tasks for today.."/>
 
+
         <button onClick={submitTodo}>Submit</button>
+        </div>
         {items.map((item, i) => (<div class="todos"><div class="item">{item}</div><div class="buttons"><button class="edit-button" onClick =  { () => editTodo(i)}>Edit</button>
       <button class="delete-button" onClick =  { () => deleteTodo(i)}>Delete</button>
      </div>
